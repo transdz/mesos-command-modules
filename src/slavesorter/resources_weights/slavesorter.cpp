a456f62c3783f744151dfc5664653a28f569fd22
djdhm <ed_dahmane@esi.dz>
2021-04-26 15:49:35 +0200

    Update slavesorter.cpp

--- src/slavesorter/resources_weights/slavesorter.cpp
+++ src/slavesorter/resources_weights/slavesorter.cpp
@@ -13,7 +13,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-#include "master/allocator/mesos/slavesorter/resources_weights/slavesorter.hpp"
+#include "slavesorter/resources_weights/slavesorter.hpp"
 
 namespace mesos {
 namespace internal {
