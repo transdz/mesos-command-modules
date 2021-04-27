ac59a987a2247554d80819200276076a7c3c2ad1
root <ed_dahmane@esi.dz>
2021-04-27 10:17:52 +0000

    explicit resource weights

--- src/slavesorter/resources_weights/slavesorter.cpp
+++ src/slavesorter/resources_weights/slavesorter.cpp
@@ -46,6 +46,10 @@ void MyResourceWeightedSlaveSorter::initialize(const Option<std::string>& slaveS
       gpuWeight = resourceWeights.get().get("gpus").value();
 
     }
+     cpuWeight = 1;
+    diskWeight = 0.2;
+    memWeight = 0.3;
+    gpuWeight= 0;
   LOG(INFO) << "Set slave sorter resource weights from arg string [" << slaveSorterResourceWeights.get() << "] weights : cpus:" << cpuWeight << " gpus:" << gpuWeight << " disk:" << diskWeight << " mem:" << memWeight;
   }else {
     cpuWeight = 1;
